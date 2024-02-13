26db3a7b9c7a59fc2d52a63285b80a3e687eec80
StefTechBreeze <stefan.radusi@tech-breeze.com>
2024-02-13 20:25:42 +0200

    update seo

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,6 +8,25 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
+          <title>Light Detail Studio</title>
+          <meta
+            name="description"
+            content="Light Details's mission is to design and implement functionally-aesthetically balanced spaces tailored to the client's personality traits."
+          ></meta>
+          <meta
+            name="keywords"
+            content="interior design, cluj-napoca, romania, 3d visualizations, randari, design interior"
+          ></meta>
+          <meta property="og:title" content="Light Detail Studio"></meta>
+          <meta
+            property="og:description"
+            content="Light Details's mission is to design and implement functionally-aesthetically balanced spaces tailored to the client's personality traits."
+          ></meta>
+          <meta
+            property="og:image"
+            content="https://lightdetail.eu/img/residential.jpg"
+          />
+          <meta property="og:url" content="https://lightdetail.eu" />
           <link rel="preload" as="image" href="/img/cover.jpg"></link>
           <link rel="preload" as="image" href="/img/about-us-cover.jpg"></link>
           <link rel="preload" as="image" href="/img/residential.jpg"></link>
