1b86afe98de7c169dd11f7843b47c24947f57965
StefTechBreeze <stefan.radusi@tech-breeze.com>
2025-09-10 14:14:49 +0300

    add conversion

--- public/script/google.js
+++ public/script/google.js
@@ -5,3 +5,9 @@ function gtag() {
 gtag("js", new Date());
 
 gtag("config", "AW-17547056259");
+
+gtag("event", "conversion", {
+  send_to: "AW-17547056259/74RxCMLZ9ZcbEIOpi69B",
+  value: 1.0,
+  currency: "RON",
+});
